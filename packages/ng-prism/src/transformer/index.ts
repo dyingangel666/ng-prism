ba220ad4ce06364aff67480654bdb64969b62c4b
Alexander Spies <alexander.spies@softgarden.de>
2026-05-04 18:35:37 +0200

    feat: add ng-prism/transformer subpath export and ng-prism-strip bin entry

---
+++ packages/ng-prism/src/transformer/index.ts
@@ -0,0 +1,8 @@
+export {
+  createShowcaseStripTransformer,
+  stripShowcaseDecorators,
+} from './showcase-strip.transformer.js';
+export {
+  stripShowcaseFromDirectory,
+  type StripResult,
+} from './strip-directory.js';
